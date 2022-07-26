ffc451dff89b730f4175c8166f65cd4a95148a54
Parvez453 <110047278+Parvez453@users.noreply.github.com>
2022-07-26 23:58:34 +0600

    Add files via upload

---
+++ lab 01/01. 453_ 49.cpp
@@ -0,0 +1,12 @@
+//Codeforced Problem No - 4A
+//Problem name - watermelon
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    (a%2==0)&&(a > 2)? cout<<"YES"<<endl:cout<<"NO"<<endl;
+    return 0;
+}
