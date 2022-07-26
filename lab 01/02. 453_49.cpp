2f0e4afcdce48ccb6b6cb7c3ee929e38ecdeb13d
Parvez453 <110047278+Parvez453@users.noreply.github.com>
2022-07-27 00:00:20 +0600

    Add files via upload

---
+++ lab 01/02. 453_49.cpp
@@ -0,0 +1,25 @@
+//Codeforces Problem No - 71A
+//Problame name - Way to long words
+
+
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+     string a;
+    cin>>a;
+    int num=a.size();
+    num>10 ? cout<<a[0]<<num-2<<a[num-1]<<endl:cout<<a<<endl;
+
+    }
+    return 0;
+
+
+}
+}
